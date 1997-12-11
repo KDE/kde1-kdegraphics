25adb4eba82d25174136795ba19a93b66a5e8030
KDE Dinosaur rich <cvsuser@kde.org>
1997-12-11 12:06:48 +0000

    Updated version number

--- kpaint/version.h
+++ kpaint/version.h
@@ -2,9 +2,9 @@
 
 #define APPNAME "kpaint"
 #define APPHELPCONTENTS "kpaint.html"
-#define APPVERSTR "Release 0.3.3"
+#define APPVERSTR "Release 0.4"
 #define APPMAJVER 0
-#define APPMINVER 3
+#define APPMINVER 4
 #define APPAUTHOR "Richard Moore"
 #define APPAUTHOREMAIL "rich@kde.org"
 
