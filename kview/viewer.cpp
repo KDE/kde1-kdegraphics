3ac3522414d7340e9540c03d05342aa95d78cbcb
KDE Dinosaur bieker <cvsuser@kde.org>
1998-12-07 17:09:07 +0000

    i18n("Couldn't load %s");

--- kview/viewer.cpp
+++ kview/viewer.cpp
@@ -667,8 +667,8 @@ void KImageViewer::loadFile( const char *file, const char *url )
 
 	if( _canvas->status() != KImageCanvas::OK ) {
 
-		QString msg = "Couldn't load ";
-		msg += url;
+		QString msg;
+		msg.sprintf(i18n("Couldn't load %s"), url);
 		message( msg );
 	}
 	else {
