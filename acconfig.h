70ccb911d3647f388337a026577cb594362b20c1
KDE Dinosaur kulow <cvsuser@kde.org>
1999-01-14 18:36:20 +0000

    CVS_SILENT: don't use config.h.bot anymore

--- acconfig.h
+++ acconfig.h
@@ -26,3 +26,32 @@
 /* Define to 1 if NLS is requested.  */
 #undef ENABLE_NLS
 
+
+#define HAVE_MEDIATOOL 1
+
+#ifndef HAVE_BOOL
+#define HAVE_BOOL
+typedef int bool;
+#ifdef __cplusplus
+const bool false = 0;
+const bool true = 1;
+#else
+#define false (bool)0;
+#define true (bool)1;
+#endif
+#endif
+#ifndef HAVE_FUNC_SETENV
+#define HAVE_FUNC_SETENV
+int setenv(const char *name, const char *value, int overwrite);
+int unsetenv(const char *name);
+#endif
+
+/*
+ * jpeg.h needs HAVE_BOOLEAN, when the system uses boolean in system
+ * headers and I'm too lazy to write a configure test as long as only
+ * unixware is related
+ */
+#ifdef _UNIXWARE
+#define HAVE_BOOLEAN
+#endif
+
